858368dacd3cb4176f01256bd23f0fe9742553c2
Rajeen Balasuriya <102136700+RajeenBalasuriya@users.noreply.github.com>
2024-04-17 14:13:30 +0530

    created-random-file-selector

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
+import java.nio.file.Path;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+// Specify the directory containing the maze files
+        String mazeDirectory = "../maze";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Create an instance of MazeFileHandler
+        MazeFileHandler fileHandler = new MazeFileHandler(mazeDirectory);
+
+        // Get a random maze file from the directory
+        Path randomMazeFile = fileHandler.getRandomMazeFile();
+        if (randomMazeFile != null) {
+            System.out.println("Selected maze file: " + randomMazeFile.getFileName());
+
+
+        } else {
+            System.out.println("No maze files found in the directory.");
         }
     }
-}
+
+    }
